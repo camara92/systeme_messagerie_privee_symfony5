108a3cff01d3c058bc206d3852812f1ebfafd430
CAMARA <88945535+camara92@users.noreply.github.com>
2023-01-21 03:33:16 +0100

    envoie de message good

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Messages;
 use App\Form\MessagesType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,13 +21,26 @@ class MessagesController extends AbstractController
     }
 
     #[Route('/send', name: 'app_send')]
-    public function send(Request $request ) :Response {
+    public function send(Request $request, EntityManagerInterface $entityManager ) :Response {
        $message = new Messages(); 
        $form = $this->createForm(MessagesType::class, $message ); 
+        // traitement du formulaire : 
+         $form->handleRequest($request);    
+       if($form->isSubmitted() && $form->isValid()){
+        $message->setSender($this->getUser());
+        $entityManager->persist($message);
+        $entityManager->flush();
+        // $em->persist($message);
+        // $em->flush();
+
+        $this->addFlash('message', 'Message envoyé avec succès.');
+
+        return $this->redirectToRoute('app_messages');
+       }
         return $this->render('messages/sender.html.twig', [
             'controller_name' => 'Envoie de message ',
             'form'=>$form->createView()
-            
+
 
         ]);
     }

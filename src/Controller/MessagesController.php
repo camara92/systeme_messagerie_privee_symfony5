15cca7671d41e0c4ecfb54d44f348750da0521f4
CAMARA <88945535+camara92@users.noreply.github.com>
2023-01-21 03:01:39 +0100

    formtype

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Messages;
+use App\Form\MessagesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,18 @@ class MessagesController extends AbstractController
     {
         return $this->render('messages/index.html.twig', [
             'controller_name' => 'MessagesController',
+        ]);
+    }
+
+    #[Route('/send', name: 'app_send')]
+    public function send(Request $request ) :Response {
+       $message = new Messages(); 
+       $form = $this->createForm(MessagesType::class, $message ); 
+        return $this->render('messages/sender.html.twig', [
+            'controller_name' => 'Envoie de message ',
+            'form'=>$form->createView()
+            
+
         ]);
     }
 }
--- src/Form/MessagesType.php
+++ src/Form/MessagesType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Messages;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,38 @@ class MessagesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('message')
-            ->add('created_at')
-            ->add('is_read')
-            ->add('sender')
-            ->add('recipient')
-        ;
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add(
+                'message',
+                TextareaType::class,
+                [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+            ->add('recipient', EntityType::class, [
+                'class' => Users::class,
+                'choice-label' => 'email',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            // ->add('created_at')
+            // ->add('is_read')
+            // ->add('sender', ) 
+            // le sender c'est l'utilisateur connefcté 
+
+            ->add('Envoyer', SubmitType::class, [
+
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

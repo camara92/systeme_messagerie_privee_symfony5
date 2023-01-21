b50fcd6a7aaeef63c5eb484cc68d2346daecc8ee
CAMARA <88945535+camara92@users.noreply.github.com>
2023-01-21 02:31:20 +0100

    formulaire de la messagerie entre les users

---
+++ src/Form/MessagesType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Messages;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessagesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('message')
+            ->add('created_at')
+            ->add('is_read')
+            ->add('sender')
+            ->add('recipient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Messages::class,
+        ]);
+    }
+}

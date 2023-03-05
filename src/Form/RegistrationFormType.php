294d7c7f1be61f7c4a6645d9aa8926942a5b2c40
CAMARA <88945535+camara92@users.noreply.github.com>
2023-03-05 10:35:18 +0100

    quelques modifications en front

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,33 +4,69 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            ->add(
+                'firstname',
+                TextType::class,
+                [
+                    'label'=>'Votre prénom',
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+            ->add(
+                'lastname',
+                TextType::class,
+                [
+                    'label'=>'Votre nom',
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'Votre email', 
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'label'=>'Votre mot de passe',
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -43,7 +79,44 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+
+
+            ->add(
+                'cp',
+                IntegerType::class,
+                [
+                    'label'=>'Code postal',
+                    'attr' => [
+                        'class' => 'form-control',
+                        'label' => 'Code postal'
+                    ]
+                ]
+            )
+            ->add(
+                'city',
+                TextType::class,
+                [
+                    'label' => 'Votre ville',
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+            ->add(
+                'userprofile',
+                TextType::class,
+                [
+                    'label' => 'Photo de profile ou lien ',
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ]
+            )
+            ->add('Valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success mt-2 container bold',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
